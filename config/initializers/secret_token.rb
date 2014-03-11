00c8eb7e1419f6a68416c31a87c95e4a31f35262
Ethan Abramson <nflethana@gmail.com>
2014-03-11 12:01:11 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SortedNews::Application.config.secret_token = '82610a6158564eef49d73885b40ea31933a7b3727676ce5b3aaf765b7950a717d4f99dd9dfbfdfeb6985c6144f29486aaed05683f5b63862bbdb69bc700a7d02'
