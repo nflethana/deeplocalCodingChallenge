3024c9270113a23958e54769e9b97677e2bf810f
Ethan Abramson <nflethana@gmail.com>
2014-03-17 10:57:56 -0400

    final commit sorts all working properly

--- app/controllers/sort_controller.rb
+++ app/controllers/sort_controller.rb
@@ -4,33 +4,37 @@ class SortController < ApplicationController
 		attr_accessor :title, :link, :comments, :page
 	end
 
-  def comments
-  	# url = "http://www.news.ycombinator.com/"
-  	# agent = Mechanize.new
-  	# agent.get(url)
-    # doc = agent.page.parser.css('td')[0]
+  # method to pull and parse from news.ycombinator.com/rss because
+  # news.ycombinator.com/ cannot be scrapped
+  def scrape
     @array = Array.new
     @homepage = Nokogiri::HTML(open("https://news.ycombinator.com/rss"))
   	@individualLinksArray = @homepage.css('comments')
   	@individualLinksArray.each { |x|
 
+  		# create new post object to store the data necessary
   		@onePost = Post.new
   		@onePost.page = x
   		@individualPage = Nokogiri::HTML(open(x))
 
+  		# parse out the number of comments for each page
   		@numComments = @individualPage.css('tr')[3].css('tr')[1].css('a')[1]
   		if @numComments != nil
   			if @numComments.text == "discuss"
-  				@numComments = 0
+  				@numComments = Integer(0)
   			else
-  				@onePost.comments = @numComments.text.gsub("comments","").gsub("comment","")
-  				#need to convert the above string to integer that way I can sort by it below
-  				puts @onePost.comments
+  				@hold = @numComments.text
+  				if @hold != nil
+  					@onePost.comments = Integer(@hold.gsub("comments","").gsub("comment",""))
+  				else 
+  					@onePost.comments = Integer(0)
+  				end
   			end
   		else
-  			@onePost.comments = 0
+  			@onePost.comments = Integer(0)
   		end
 
+  		# parse out the link domain
   		@postLink = @individualPage.css('tr')[3].css('tr').css('td')[1].css('span').text
   		if @postLink != nil
   			@postLink.gsub!("(","")
@@ -42,18 +46,43 @@ class SortController < ApplicationController
   		else
   		end
 
+  		# parse out the post headline
   		@postTitle = @individualPage.css('tr')[3].css('tr')[0].css('a[href]').text
   		@onePost.title = @postTitle
 
   		@array.push(@onePost)
   	}
-  	puts @array.each{ |x| puts x.comments }
-  	#@array.sort! { |x,y| Integer (x.comments) <=> Integer(y.comments) }
+  	return @array
+  end
+
+  def comments
+  	@arr = scrape
+  	# logging:
+  	# @arr.each {|x| 
+  	# 	if x.comments == nil 
+  	# 		puts "found nil comments"
+  	# 		x.comments = Integer(0)
+  	# 	else 
+  	# 		puts x.comments
+  	# 	end}
+  	# puts "------------"
+  	@arr.sort!{|x,y| y.comments <=> x.comments}
+  	# logging: @arr.each {|x| puts x.comments}
   end
 
   def domain
+  	@arr = scrape
+  	# logging: @arr.each {|x| puts x.link}
+  	# puts "------------"
+  	@arr.sort! {|x,y| x.link <=> y.link }
+  	# loggging: @arr.each {|x| puts x.link}
   end
 
   def headline_length
+  	@arr = scrape
+  	# loggging: @arr.each {|x| puts x.title}
+  	# puts "------------"
+  	@arr.sort! {|x,y| x.title.length <=> y.title.length }
+  	# logging: @arr.each {|x| puts x.title}
   end
 end
